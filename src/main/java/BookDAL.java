1ea08787aa79d84c58c4114c9547c71e0f54050b
djordjebla <blagojevesl@gmail.com>
2023-09-06 18:02:46 +0200

    Created ControllerServlet.java (need tests), switched to mockito for testing

--- src/main/java/BookDAL.java
+++ src/main/java/BookDAL.java
@@ -1,19 +1,59 @@
 import models.Book;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookDAL {
 
-    private final DataSource dataSource;
+    private String jdbcURL;
+    private String jdbcUsername;
+    private String jdbcPassword;
+    private Connection jdbcConnection;
 
-    public BookDAL(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public BookDAL(String jdbcURL, String jdbcUsername, String jdbcPassword) {
+        this.jdbcURL = jdbcURL;
+        this.jdbcUsername = jdbcUsername;
+        this.jdbcPassword = jdbcPassword;
+    }
+
+    public BookDAL(Connection mockConnection) {
+        this.jdbcConnection = mockConnection;
+
+    }
+
+    protected void connect() throws SQLException {
+        if (jdbcConnection == null || jdbcConnection.isClosed()) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            jdbcConnection = DriverManager.getConnection(
+                    jdbcURL, jdbcUsername, jdbcPassword);
+        }
+    }
+
+    protected void disconnect() throws SQLException {
+        if (jdbcConnection != null && !jdbcConnection.isClosed()) {
+            jdbcConnection.close();
+        }
+    }
+
+    public boolean insertBook(Book book) throws SQLException {
+        String sql = "INSERT INTO book (title, author, price) VALUES (?, ?, ?)";
+        connect();
+
+        PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+        statement.setString(1, book.getTitle());
+        statement.setString(2, book.getAuthor());
+        statement.setFloat(3, book.getPrice());
+
+        boolean rowInserted = statement.executeUpdate() > 0;
+        statement.close();
+        disconnect();
+        return rowInserted;
     }
 
     public List<Book> listAllBooks() throws SQLException {
@@ -21,80 +61,82 @@ public class BookDAL {
 
         String sql = "SELECT * FROM book";
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql);
-             ResultSet resultSet = statement.executeQuery()) {
+        connect();
 
-            while (resultSet.next()) {
-                int id = resultSet.getInt("book_id");
-                String title = resultSet.getString("title");
-                String author = resultSet.getString("author");
-                float price = resultSet.getFloat("price");
+        Statement statement = jdbcConnection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
 
-                Book book = new Book(id, title, author, price);
-                listBook.add(book);
-            }
+        while (resultSet.next()) {
+            int id = resultSet.getInt("book_id");
+            String title = resultSet.getString("title");
+            String author = resultSet.getString("author");
+            float price = resultSet.getFloat("price");
+
+            Book book = new Book(id, title, author, price);
+            listBook.add(book);
         }
+
+        resultSet.close();
+        statement.close();
+
+        disconnect();
 
         return listBook;
-    }
-
-    public boolean insertBook(Book book) throws SQLException {
-        String sql = "INSERT INTO book (title, author, price) VALUES (?, ?, ?)";
-
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, book.getTitle());
-            statement.setString(2, book.getAuthor());
-            statement.setFloat(3, book.getPrice());
-
-            return statement.executeUpdate() > 0;
-        }
     }
 
     public boolean deleteBook(Book book) throws SQLException {
         String sql = "DELETE FROM book where book_id = ?";
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, book.getId());
+        connect();
 
-            return statement.executeUpdate() > 0;
-        }
+        PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+        statement.setInt(1, book.getId());
+
+        boolean rowDeleted = statement.executeUpdate() > 0;
+        statement.close();
+        disconnect();
+        return rowDeleted;
     }
 
     public boolean updateBook(Book book) throws SQLException {
-        String sql = "UPDATE book SET title =?, author =?, price =?";
+        String sql = "UPDATE book SET title = ?, author = ?, price = ?";
         sql += " WHERE book_id = ?";
+        connect();
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, book.getTitle());
-            statement.setString(2, book.getAuthor());
-            statement.setFloat(3, book.getPrice());
-            statement.setInt(4, book.getId());
+        PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+        statement.setString(1, book.getTitle());
+        statement.setString(2, book.getAuthor());
+        statement.setFloat(3, book.getPrice());
+        statement.setInt(4, book.getId());
 
-            return statement.executeUpdate() > 0;
-        }
+        boolean rowUpdated = statement.executeUpdate() > 0;
+        statement.close();
+        disconnect();
+        return rowUpdated;
     }
 
     public Book getBook(int id) throws SQLException {
         Book book = null;
         String sql = "SELECT * FROM book WHERE book_id = ?";
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, id); // Set the parameter value
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    String title = resultSet.getString("title");
-                    String author = resultSet.getString("author");
-                    float price = resultSet.getFloat("price");
+        connect();
 
-                    book = new Book(id, title, author, price);
-                }
-            }
+        PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+        statement.setInt(1, id);
+
+        ResultSet resultSet = statement.executeQuery();
+
+        if (resultSet.next()) {
+            String title = resultSet.getString("title");
+            String author = resultSet.getString("author");
+            float price = resultSet.getFloat("price");
+
+            book = new Book(id, title, author, price);
         }
+
+        resultSet.close();
+        statement.close();
+
         return book;
     }
 
--- src/test/java/BookDALTest.java
+++ src/test/java/BookDALTest.java
@@ -3,257 +3,50 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookDALTest {
-    private DataSource dataSource;
+    @Mock
+    private Connection mockConnection;
+
+    @Mock
+    private PreparedStatement mockPreparedStatement;
+
     private BookDAL bookDAL;
 
-    @BeforeEach
-    public void setUp() {
-        dataSource = mock(DataSource.class); // Use a mock DataSource for testing
-        bookDAL = new BookDAL(dataSource);
+    @Before
+    public void setUp() throws SQLException {
+        MockitoAnnotations.initMocks(this);
+
+        // Initialize the BookDAL with the mockConnection
+        bookDAL = new BookDAL(mockConnection);
     }
 
     @After
     public void tearDown() throws SQLException {
-        verify(dataSource.getConnection()).close();
+        // Clean up and close any resources
     }
 
     @Test
-    public void testListAllBooks() throws SQLException {
-        // Create a list of books that you expect to be returned from the database
-        List<Book> expectedBooks = new ArrayList<>();
-        expectedBooks.add(new Book(1, "Book1", "Author1", 19.99f));
-        expectedBooks.add(new Book(2, "Book2", "Author2", 24.99f));
+    public void testInsertBook() throws SQLException {
+        Book testBook = new Book("Test Title", "Test Author", 19.99f);
 
-        // Mock the behavior of the DataSource and Connection
-        Connection connection = mock(Connection.class);
-        when(dataSource.getConnection()).thenReturn(connection);
+        Mockito.when(mockConnection.prepareStatement(Mockito.anyString())).thenReturn(mockPreparedStatement);
+        Mockito.when(mockPreparedStatement.executeUpdate()).thenReturn(1);
 
-        // Mock the behavior of PreparedStatement and ResultSet
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
-        ResultSet resultSet = mock(ResultSet.class);
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        boolean insertionResult = bookDAL.insertBook(testBook);
 
-        // Mock the ResultSet to return data when next() is called
-        when(resultSet.next()).thenReturn(true, true, false); // Two rows
-        when(resultSet.getInt("book_id")).thenReturn(1, 2);
-        when(resultSet.getString("title")).thenReturn("Book1", "Book2");
-        when(resultSet.getString("author")).thenReturn("Author1", "Author2");
-        when(resultSet.getFloat("price")).thenReturn(19.99f, 24.99f);
-
-        // Perform the test
-        List<Book> actualBooks = bookDAL.listAllBooks();
-
-        // Verify that the actual result matches the expected result
-        assertEquals(expectedBooks, actualBooks);
-    }
-
-    @Test
-    public void testInsertBook_Success() throws SQLException {
-        // Mock the necessary objects
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
-
-        // Configure the dataSource to return the mock connection
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        when(preparedStatement.executeUpdate()).thenReturn(1);
-
-        // Create a sample book
-        Book book = new Book("Sample title", "Sample Author", 10.22f);
-
-        // Call the insertBook method
-        boolean result = bookDAL.insertBook(book);
-
-        // Verify that the SQL query was executed with the correct parameters
-        verify(preparedStatement).setString(1, "Sample title");
-        verify(preparedStatement).setString(2, "Sample Author");
-        verify(preparedStatement).setFloat(3, 10.22f);
-
-        // Verify that the connection and prepared statement were closed
-        verify(preparedStatement).close();
-        verify(connection).close();
-
-        // Check if the assertion was successful
-        assertTrue(result);
-    }
-
-    @Test
-    public void testInsertBook_Failure() throws SQLException {
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
-
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        when(preparedStatement.executeUpdate()).thenThrow(new SQLException("Insertion failed"));
-
-        Book book = new Book("Sample title", "Sample author", 10.22f);
-
-        boolean result = bookDAL.insertBook(book);
-
-        verify(preparedStatement).setString(1, "Sample title");
-        verify(preparedStatement).setString(2, "Sample author");
-        verify(preparedStatement).setFloat(3, 10.22f);
-
-        verify(preparedStatement).close();
-        verify(connection).close();
-
-        assertFalse(result);
-    }
-
-    @Test
-    public void testDeleteBook_Success() throws SQLException {
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
-
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        when(preparedStatement.executeUpdate()).thenReturn(1);
-
-        Book book = new Book();
-        book.setId(1);
-
-        boolean result = bookDAL.deleteBook(book);
-
-        verify(preparedStatement).setInt(1, 1);
-
-        verify(preparedStatement).close();
-        verify(connection).close();
-
-        assertTrue(result);
-    }
-
-    @Test
-    public void testDeleteBook_Failure() throws SQLException {
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
-
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        when(preparedStatement.executeUpdate()).thenThrow(new SQLException("Deletion failed"));
-
-        Book book = new Book();
-        book.setId(1);
-
-        boolean result = bookDAL.deleteBook(book);
-
-        verify(preparedStatement).setInt(1, 1);
-
-        verify(preparedStatement).close();
-        verify(connection).close();
-
-        assertFalse(result);
-    }
-
-    @Test
-    public void testUpdateBook_Success() throws SQLException {
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
-
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        when(preparedStatement.executeUpdate()).thenReturn(1);
-
-        Book book = new Book();
-        book.setId(1);
-        book.setTitle("Updated Title");
-        book.setAuthor("Updated Author");
-        book.setPrice(10.22f);
-
-        boolean result = bookDAL.updateBook(book);
-
-        verify(preparedStatement).setString(1, "Updated Title");
-        verify(preparedStatement).setString(2, "Updated Author");
-        verify(preparedStatement).setFloat(3, 10.22f);
-        verify(preparedStatement).setInt(4, 1);
-
-        verify(preparedStatement).close();
-        verify(connection).close();
-
-        assertTrue(result);
-    }
-
-    @Test
-    public void testUpdateBook_Failure() throws SQLException {
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
-
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        when(preparedStatement.executeUpdate()).thenThrow(new SQLException("Update failed"));
-
-        Book book = new Book();
-        book.setId(1);
-        book.setTitle("Updated Title");
-        book.setAuthor("Updated Author");
-        book.setPrice(10.22f);
-
-        boolean result = bookDAL.updateBook(book);
-
-        verify(preparedStatement).setString(1, "Updated Title");
-        verify(preparedStatement).setString(2, "Updated Author");
-        verify(preparedStatement).setFloat(3, 10.22f);
-        verify(preparedStatement).setInt(4, 1);
-
-        verify(preparedStatement).close();
-        verify(connection).close();
-
-        assertFalse(result);
-    }
-
-    @Test
-    public void testGetBook_ExistingBook() throws SQLException {
-        int id = 1;
-        Connection connection = mock(Connection.class);
-        PreparedStatement statement = mock(PreparedStatement.class);
-        ResultSet resultSet = mock(ResultSet.class);
-
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(statement);
-        when(statement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(true);
-        when(resultSet.getInt("book_id")).thenReturn(id);
-        when(resultSet.getString("title")).thenReturn("Test Title");
-        when(resultSet.getString("author")).thenReturn("Test Author");
-        when(resultSet.getFloat("price")).thenReturn(19.99f);
-
-        Book result = bookDAL.getBook(id);
-
-        assertNotNull(result);
-        assertEquals(id, result.getId());
-        assertEquals("Test Title", result.getTitle());
-        assertEquals("Test Author", result.getAuthor());
-        assertEquals(19.99f, result.getPrice(), 0.01); // Comparing floating-point numbers with a small epsilon
-    }
-
-    @Test
-    public void testGetBook_NonExistingBook() throws SQLException {
-        int id = 2;
-        Connection connection = mock(Connection.class);
-        PreparedStatement statement = mock(PreparedStatement.class);
-        ResultSet resultSet = mock(ResultSet.class);
-
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(statement);
-        when(statement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(false);
-
-        Book result = bookDAL.getBook(id);
-
-        assertNull(result);
+        assertTrue(insertionResult);
     }
 
 }
