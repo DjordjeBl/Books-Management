2b5430dfdb117ec9df6e061421808ccd75e74e34
djordjebla <blagojevesl@gmail.com>
2023-09-08 18:54:44 +0200

    added tests for deleting books, updating, and getting

--- src/test/java/BookDALTest.java
+++ src/test/java/BookDALTest.java
@@ -2,6 +2,7 @@ import models.Book;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,7 +32,6 @@ public class BookDALTest {
         try (AutoCloseable mocks = MockitoAnnotations.openMocks(this)) {
             bookDAL = new BookDAL(mockConnection);
 
-            // Configure the mock objects
             when(mockConnection.prepareStatement(anyString())).thenReturn(mockStatement);
             when(mockConnection.createStatement()).thenReturn(mockStatement);
             when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
@@ -83,22 +83,18 @@ public class BookDALTest {
 
     @Test
     public void testListAllBooks() throws SQLException {
-        // Create a sample list of books to return as mock data
         List<Book> expectedBooks = new ArrayList<>();
         expectedBooks.add(new Book(1, "Sample Title 1", "Sample Author 1", 19.99f));
         expectedBooks.add(new Book(2, "Sample Title 2", "Sample Author 2", 29.99f));
 
-        // Configure the mock ResultSet to return data when next() is called
         when(mockResultSet.next()).thenReturn(true, true, false);
         when(mockResultSet.getInt("book_id")).thenReturn(1, 2);
         when(mockResultSet.getString("title")).thenReturn("Sample Title 1", "Sample Title 2");
         when(mockResultSet.getString("author")).thenReturn("Sample Author 1", "Sample Author 2");
         when(mockResultSet.getFloat("price")).thenReturn(19.99f, 29.99f);
 
-        // Call the listAllBooks method
         List<Book> result = bookDAL.listAllBooks();
 
-        // Verify that the result matches the expected data
         assertEquals(expectedBooks.size(), result.size());
         for (int i = 0; i < expectedBooks.size(); i++) {
             Book expectedBook = expectedBooks.get(i);
@@ -119,5 +115,89 @@ public class BookDALTest {
         verify(mockResultSet, times(2)).getFloat("price");
         verify(mockResultSet, times(1)).close();
         verify(mockStatement, times(1)).close();
+    }
+
+    @Test
+    public void deleteBookTest_Success() throws SQLException {
+        when(mockStatement.executeUpdate()).thenReturn(1);
+
+        int bookIdToDelete = 1; // Replace with the actual bookId
+
+        try {
+            boolean isDeleted = bookDAL.deleteBook(bookIdToDelete);
+            assertTrue(isDeleted);
+        } catch (SQLException e) {
+            fail("Exception should not be thrown during the test.");
+        }
+    }
+
+    @Test
+    public void deleteBookTest_Failure() throws SQLException {
+        when(mockStatement.executeUpdate()).thenReturn(0);
+
+        int bookIdToDelete = 1;
+
+        boolean isDeleted = bookDAL.deleteBook(bookIdToDelete);
+        assertFalse(isDeleted);
+    }
+
+    @Test
+    public void updateBookTest_Success() throws SQLException {
+        Book bookToUpdate = new Book();
+        bookToUpdate.setId(1);
+        bookToUpdate.setTitle("Updated Title");
+        bookToUpdate.setAuthor("Updated Author");
+        bookToUpdate.setPrice(19.99f);
+
+        Mockito.when(mockStatement.executeUpdate()).thenReturn(1);
+        boolean isUpdated = bookDAL.updateBook(bookToUpdate);
+        assertTrue(isUpdated);
+    }
+
+    @Test
+    public void updateBookTest_Failure() throws SQLException {
+        Book bookToUpdate = new Book();
+        bookToUpdate.setId(1);
+        bookToUpdate.setTitle("Updated Title");
+        bookToUpdate.setAuthor("Updated Author");
+        bookToUpdate.setPrice(19.99f);
+
+        Mockito.when(mockStatement.executeUpdate()).thenReturn(0);
+        boolean isUpdated = bookDAL.updateBook(bookToUpdate);
+        assertFalse(isUpdated);
+    }
+
+    @Test
+    public void getBookTest_Success() throws SQLException {
+        int bookIdToRetrieve = 1;
+        String expectedTitle = "Sample Title";
+        String expectedAuthor = "Sample Author";
+        float expectedPrice = 29.99f;
+
+        Mockito.when(mockStatement.executeQuery()).thenReturn(mockResultSet);
+        Mockito.when(mockResultSet.next()).thenReturn(true);
+        Mockito.when(mockResultSet.getString("title")).thenReturn(expectedTitle);
+        Mockito.when(mockResultSet.getString("author")).thenReturn(expectedAuthor);
+        Mockito.when(mockResultSet.getFloat("price")).thenReturn(expectedPrice);
+
+        Book retrievedBook = bookDAL.getBook(bookIdToRetrieve);
+
+        assertNotNull(retrievedBook);
+        assertEquals(bookIdToRetrieve, retrievedBook.getId());
+        assertEquals(expectedTitle, retrievedBook.getTitle());
+        assertEquals(expectedAuthor, retrievedBook.getAuthor());
+        assertEquals(expectedPrice, retrievedBook.getPrice(), 0.01);
+    }
+
+    @Test
+    public void getBookTest_Failure() throws SQLException {
+        int bookIdToRetrieve = 1;
+
+        Mockito.when(mockStatement.executeQuery()).thenReturn(mockResultSet);
+        Mockito.when(mockResultSet.next()).thenReturn(false);
+
+        Book retrievedBook = bookDAL.getBook(bookIdToRetrieve);
+
+        assertNull(retrievedBook);
     }
 }
